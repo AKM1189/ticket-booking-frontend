79ea488972c2a5c35228575a0e9f6939a1b28411
Aung Kaung Myat <109449422+AKM1189@users.noreply.github.com>
2025-07-29 11:49:01 +0630

    Update api.ts

--- src/api/api.ts
+++ src/api/api.ts
@@ -12,7 +12,7 @@ export const api: AxiosInstance = axios.create({
   },
   withCredentials: true,
 });
-
+/////////////////
 api.interceptors.request.use(
   (config) => {
     const accessToken = Cookies.get("accessToken");
